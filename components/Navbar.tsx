cbeef989fb7392a1c72a4d693c19b34fda1a7b3e
izabelaoska <91794571+izabelaoska@users.noreply.github.com>
2022-07-28 13:28:28 +0100

    feat: Sign in component and button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,20 @@
 import { Basket } from "./Basket"
 import { Notifications } from "./Notifications"
 import { Search } from "./Search"
+import { SignIn } from "./SignIn"
 
 export const Navbar = () => {
   return (
     <header className="bg-gray-900 text-white shadow-xl fixed w-full">
       <nav className="container px-4 mx-auto navbar">
-        <div className="navbar-start">
+        <div className="navbar-start gap-8">
           <a className="normal-case text-xl">E-Commerce</a>
         </div>
-        <div className="navbar-end">
+        <div className="navbar-end gap-2">
           <Search />
           <Notifications />
           <Basket />
+          <SignIn />
         </div>
       </nav>
     </header>
---
+++ components/SignIn.tsx
@@ -0,0 +1,20 @@
+export const SignIn = () => {
+  return (
+    <div className="dropdown dropdown-end">
+      <label
+        tabIndex={0}
+        className="m-1 btn bg-gray-900 border-none hover:bg-gray-900"
+      >
+        Sign in
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 text-gray-900"
+      >
+        <li>
+          <a>Register</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
