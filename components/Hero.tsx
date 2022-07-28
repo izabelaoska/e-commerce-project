03f370eb73c7af9bc43ba70468e0ec50652366ea
izabelaoska <91794571+izabelaoska@users.noreply.github.com>
2022-07-28 17:04:26 +0100

    feat: NewProduct component created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,17 +6,17 @@ export const Hero = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">
-            Hello there
+            Hello there beautiful!
           </h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat
-            fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id
-            nisi.
+            Coffee-based skincare to leave you smooth and
+            perky!
           </p>
-          <button className="btn btn-primary">
-            Get Started
-          </button>
+          <div className="flex justify-center gap-3">
+            <button className="btn btn-primary">
+              Shop now
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren } from "react"
 import { Hero } from "./Hero"
 import { Navbar } from "./Navbar"
+import { NewProduct } from "./NewProduct"
 
 export const Layout: React.FC<PropsWithChildren> = ({
   children,
@@ -9,7 +10,9 @@ export const Layout: React.FC<PropsWithChildren> = ({
     <div className="min-h-screen h-1">
       <Navbar />
       <main className="container px-4 mx-auto h-full pt-32">
-        {children} <Hero />
+        {children}
+        <Hero />
+        <NewProduct />
       </main>
     </div>
   )
